f08c61647009ba5bbf176db4803f21c38b7f9290
Itter Nadya <ItterNadya@gmail.com>
2022-02-08 21:43:55 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    static final int countTreads = 64;
+    static final int port = 9999;
+
+    public static void main(String[] args) {
+        Server server = new Server(countTreads);
+        server.startServer(port);
+    }
+}
+
+
